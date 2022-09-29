12888b87f915dbcf17991938037a68d770406608
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-09-29 22:48:16 +0600

    update ui

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -5,7 +5,7 @@ const Exercises = (props) => {
   const { exercises, handleAddToList } = props;
   return (
     <div className="container-fluid">
-      <h3 className="mb-5">Simple exercises to keep your body fit</h3>
+      <h3 className="mb-5">Simple exercises to keep your body fit </h3>
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
         {exercises.map((exercise) => (
           <Exercise
