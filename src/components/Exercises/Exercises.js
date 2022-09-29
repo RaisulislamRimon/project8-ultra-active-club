8a863fe66fa37e1d11edc8eca56cdbfc71bdd378
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-09-29 08:28:37 +0600

    exercises array mapped and send data to Exercise

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -13,6 +13,9 @@ const Exercises = () => {
   return (
     <div className="container-fluid">
       <h3>Select today's exercise</h3>
+      {exercises.map((exercise) => (
+        <Exercise exercise={exercise}></Exercise>
+      ))}
     </div>
   );
 };
